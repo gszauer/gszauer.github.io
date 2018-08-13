aedd4e7726cc558b8da00e1024a10f7b307af385
Gabor Szauer <git@szauer.net>
2018-08-13 02:38:28 -0700

    Reconsiled to matrix

--- MathExperiments/xform.js
+++ MathExperiments/xform.js
@@ -52,22 +52,7 @@ function DetachXFormCopy(t) {
 }
 
 function ToMatrix(transform) {
-	var x = Q_Mul_V3(transform.rotation, [1, 0, 0]);
-	var y = Q_Mul_V3(transform.rotation, [0, 1, 0]);
-	var z = Q_Mul_V3(transform.rotation, [0, 0, 1]);
-
-	x = V3_Mul_F(x, transform.scale[0])
-	y = V3_Mul_F(y, transform.scale[1])
-	z = V3_Mul_F(z, transform.scale[2])
-
-	var t = transform.position;
-
-	return [
-		x[0], x[1], x[2], 0,
-		y[0], y[1], y[2], 0,
-		z[0], z[1], z[2], 0,
-		t[0], t[1], t[2], 1
-	]
+	return PartsToMatrix(transform.position, transform.rotation, transform.scale)
 }
 
 function PartsToMatrix(position, rotation, scale) {
