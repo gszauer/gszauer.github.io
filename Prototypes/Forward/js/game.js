40facfe1e71219a4a7df7dc9e72a790327e8478a
gszauer <git@gabormakesgames.com>
2024-04-15 12:20:07 -0700

    Updated resolution for ios. Hopefully it works.

--- Prototypes/Forward/js/game.js
+++ Prototypes/Forward/js/game.js
@@ -1,10 +1,10 @@
 import SceneDungeon from './SceneDungeon.js'
 
 const config = {
-    width: 1024,
-    hheight: 2048,
-    //width: 1976,
-    //height: 3952,
+    //width: 1024,
+    //hheight: 2048,
+    width: 2048,
+    height: 4096,
     backgroundColor: '#1d1d1d',
     type: Phaser.AUTO,
     parent: 'phaser-game',
