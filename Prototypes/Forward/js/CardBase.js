4f4e323bd5881513f1e6a9f5fe99313868ebf38e
gszauer <git@gabormakesgames.com>
2024-04-20 01:13:37 -0700

    Updated how cards work

--- Prototypes/Forward/js/CardBase.js
+++ Prototypes/Forward/js/CardBase.js
@@ -2,7 +2,7 @@ export default class CardBase extends Phaser.GameObjects.Container {
     static rotationCounter = 0;
 
     constructor(data) {
-        const { scene, x, y, name, sprite, depth} = data;
+        const { scene, x, y, name, sprite, depth, value, type} = data;
 
         let set = 'set1';
         if (!scene.set1.includes(sprite)) {
@@ -17,20 +17,29 @@ export default class CardBase extends Phaser.GameObjects.Container {
 
         const nameText = new Phaser.GameObjects.BitmapText(scene, 0,0, scene.cardNameFont, name, scene.cardNameFontSize, Phaser.GameObjects.BitmapText.ALIGN_CENTER);
         
-        super(scene, x, y, [faceSprite, footerSprite, nameText]);
+        const valueSprite = scene.add.sprite(205, 2, scene.GetSet("Value.png"), "Value.png");
+        valueSprite.setOrigin(0, 0);
+
+        const valueText = new Phaser.GameObjects.BitmapText(scene, 0,0, 
+            scene.cardValueFont, value, scene.cardValueFontSize, 
+            Phaser.GameObjects.BitmapText.ALIGN_CENTER);
+
+        super(scene, x, y, [faceSprite, footerSprite, valueSprite, nameText, valueText]);
 
         this.depth = depth;
         this.scene = scene;
         this.sprite = sprite;
-
+        this.valueText = valueText;
         this.footerSprite = footerSprite;
         this.faceSprite = faceSprite;
         this.nameText = nameText;
+        this.valueSprite = valueSprite;
 
         this.Name = name;
+        this.Value = value;
 
-        this.setSize( footerSprite.width,  footerSprite.height + faceSprite.height); // TODO: footersprite.width, footerSprite.height + facesprite.height
-        this.setScale(0.92, 0.92); // To give wiggle room for rotation
+        this.setSize( footerSprite.width,  footerSprite.height + faceSprite.height); 
+        //this.setScale(0.92, 0.92); // To give wiggle room for rotation
 
         const maxRotationDegrees = 3;
         if (CardBase.rotationCounter++ % 2 == 0) {
@@ -54,5 +63,32 @@ export default class CardBase extends Phaser.GameObjects.Container {
         this.nameText.setTint(0xe1b95c);
         this.nameText.x = this.footerSprite.width / 2 - this.nameText.width / 2;
         this.nameText.y = this.footerSprite.y + this.footerSprite.height / 2 - this.nameText.height / 2;
+    }
+
+    set Value(newValue) {
+        this.value = newValue;
+        this.valueText.text = this.value;
+        this.valueText.setTint(0);
+        this.valueText.x = this.valueSprite.x + this.valueSprite.width / 2 - this.valueText.width / 2;
+        const valueSquareHeight = 107; // not: this.valueSprite.height
+        this.valueText.y = this.valueSprite.y + valueSquareHeight / 2 - this.valueText.height / 2;
+
+        if (newValue < 0) {
+            this.valueSprite.visible = false;
+            this.valueText.visible = false;
+        }
+        else {
+            this.valueSprite.visible = true;
+            this.valueText.visible = true;
+        }
+    }
+
+    SetVisibility(newValue) {
+        this.faceSprite.visible = newValue;
+        this.footerSprite.visible = newValue;
+        this.footerSprite.visible = newValue;
+        this.nameText.visible = newValue;
+        this.valueSprite.visible = newValue;
+        this.valueText.visible = newValue;
     }
 }
--- Prototypes/Forward/js/SceneDungeon.js
+++ Prototypes/Forward/js/SceneDungeon.js
@@ -1,7 +1,4 @@
 import CardBase from "./CardBase.js";
-import CardMonster from "./CardMonster.js";
-import CardPlayer from "./CardPlayer.js";
-import HudDungeon from "./HudDungeon.js";
 
 export default class SceneDungeon extends Phaser.Scene {
     constructor() {
@@ -12,6 +9,7 @@ export default class SceneDungeon extends Phaser.Scene {
         this.load.atlas('set1', 'assets/set1.png', 'assets/set1.json');
         this.load.bitmapFont('morgamon', 'assets/morgamon_0.png', 'assets/morgamon.fnt');
         this.load.bitmapFont('magicstary', 'assets/magicstary_0.png', 'assets/magicstary.fnt');
+        this.load.image('background', 'assets/background.png');
         
         this.cardNameFont = 'morgamon';
         this.cardValueFont = 'magicstary';
@@ -30,8 +28,28 @@ export default class SceneDungeon extends Phaser.Scene {
         const cardHeight = 460;
         const cardPadding = 25;
         const padLeft = 10; // Add to card padding on left to center playing field
-        const padTop = -240;
+        const padTop = -130;
 
+        const backgroundImage = this.add.image(0, 0, 'background');
+        backgroundImage.setOrigin(0, 0);
+
+        const cogSprite = this.add.sprite(0, 2048, this.GetSet("Cog.png"), "Cog.png");
+        cogSprite.x += cogSprite.width / 2 + cardPadding;
+        cogSprite.y -= cogSprite.height / 2 + cardPadding;
+        cogSprite.setScale(0.90, 0.90);
+
+        const coinSprite = this.add.sprite(1024,2048, this.GetSet("Coin.png"), "Coin.png");
+        coinSprite.x -= coinSprite.width / 2 + cardPadding;
+        coinSprite.y -= coinSprite.height / 2 + cardPadding;
+        coinSprite.setScale(0.98, 0.98);
+
+        const coinText = this.add.bitmapText(0, 0, this.cardValueFont, "-", this.cardValueFontSize, Phaser.GameObjects.BitmapText.ALIGN_CENTER);
+        coinText.x = (coinSprite.x) - (coinText.width / 2) - 20;
+        coinText.y = (coinSprite.y) - (coinText.height / 2) + 5;
+        coinText.setTint(0);
+
+
+        const monsters = [];
         for (let i = 0; i < numColumns; ++i) {
             for (let j = 0; j < numRows; ++j) {
                 const x = i * cardWidth + cardPadding + cardPadding * i + padLeft;
@@ -41,7 +59,7 @@ export default class SceneDungeon extends Phaser.Scene {
                 let randomValue = Math.floor(Math.random() * 21) + 1;
                 let randomType = "random"; // TODO: monster, heal, shield, sword, chest, empty
                
-                new CardMonster({
+                let monster = new CardBase({
                     scene: this, 
                     x: x, y: y, 
                     name: this.names1[randomCard],
@@ -50,33 +68,58 @@ export default class SceneDungeon extends Phaser.Scene {
                     value: randomValue,
                     type: randomType 
                 });
+                monsters.push(monster);
             }
         }
 
-        const playerY = numRows * cardHeight + cardPadding + cardPadding * numRows + padTop + 100;
+        const playerY = numRows * cardHeight + cardPadding + 
+                        cardPadding * numRows + padTop + 150;
         const playerX = cardWidth + cardPadding + cardPadding + padLeft;
 
-        const hud = new HudDungeon({
-            scene: this,
-            x: 0, y:  playerY + cardHeight + cardPadding * 2,
-            cardPadding: cardPadding
-        });
-
-        const player = new CardPlayer({
+        const player = new CardBase({
             scene: this, 
             x: playerX, y: playerY, 
             name: "The Fool",
             sprite: "TheFool.png",
             depth: 1,
-            health: 10,
-            maxHealth: 10,
-            shield: 2,
-            coins: 0,
-            hud: hud
+            value: 10,
+            type: "fool" 
         });
 
-        this.hud = hud;
         this.player = player;
+        this.monsters = monsters;
+        this.cogSprite = cogSprite;
+        this.coinText = coinText;
+        this.coinSprite = coinSprite;
+
+
+        this.Coins =Math.floor(Math.random() * 21) + 1;
+
+
+        // Debug code
+        /*var keyObj = this.input.keyboard.addKey('H');
+        keyObj.on('up', function(event) { 
+            player.SetVisibility(false);
+            for (let i = 0, size = monsters.length; i < size; ++i) {
+                monsters[i].SetVisibility(false);
+            }
+            
+        });
+        var keyObj = this.input.keyboard.addKey('S');
+        keyObj.on('up', function(event) { 
+            player.SetVisibility(true);
+            for (let i = 0, size = monsters.length; i < size; ++i) {
+                monsters[i].SetVisibility(true);
+            }
+        });*/
+    }
+
+    set Coins(newValue) {
+        if (newValue < 0) { newValue = 0; }
+        this.coins = newValue;
+        this.coinText.text = newValue;
+        this.coinText.x = (this.coinSprite.x) - (this.coinText.width / 2) - 20;
+        this.coinText.y = (this.coinSprite.y) - (this.coinText.height / 2) + 5;
     }
 
     GetSet(spriteName) {
